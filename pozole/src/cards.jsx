ebc19c2cdc46f8a3939995afe9df69d63905bbf6
POZOLEBLANC0 <240597@utxicotepec.edu.mx>
2026-02-03 16:55:29 -0600

    Props

--- pozole/src/cards.jsx
+++ pozole/src/cards.jsx
@@ -7,53 +7,62 @@ import Alimentacion from './assets/alimentacion.jpg';
 function Cards() {
     return (
         <div className="cards-container">
-            <Card1 />
-            <Card2 />
-            <Card3 />
-            <Card4 />
+            <Card1 name = 'Lesiones' descripcion ='Dolor de rodilla'/>
+            <Card2 name='Autocuidado' descripcion='Meditacion'/>
+            <Card3 name ='Deporte' descripcion='Fubol'/>
+            <Card4 name='Alimentacion' descripcion='Comida' saludarfunc = {saludar}/>
         </div>
     )
 }
 
 
-function Card1() {
+function Card1(props) {
     return (
         <div className="card1">
-            <img src={Lesiones} alt="Lesiones" className="card-image" />
-            <h3>Lesiones Comunes</h3>
-            <p>Información sobre las lesiones más comunes y cómo tratarlas.</p>
+            <img src={Lesiones} alt={props.name} className="card-image" />
+            <h3>{props.name}</h3>
+            <p>{props.descripcion}</p>
         </div>
     )
 
 }
 
-function Card2() {
+function Card2(props) {
     return (
         <div className="card2">
-            <img src={Autocuiaddo} alt="Autocuidado" className="card-image" />
-            <h3>Autocuidado</h3>
-            <p>Consejos y técnicas para el autocuidado físico y mental.</p>
+            <img src={Autocuiaddo} alt={props.name} className="card-image" />
+            <h3>{props.name}</h3>
+            <p>{props.descripcion}</p>
         </div>
     )
 }
 
-function Card3() {
+function Card3(props) {
     return (
         <div className="card3">
-            <img src={Deporte} alt="Deporte y Ejercicio" className="card-image" />
-            <h3>Deporte y Ejercicio</h3>
-            <p>Guías para mantenerse activo y saludable a través del deporte.</p>
+            <img src={Deporte} alt={props.name} className="card-image" />
+            <h3>{props.name}</h3>
+            <p>{props.descripcion}</p>
         </div>
         )
     }
-function Card4() {
+function Card4(props) {
     return (
         <div className="card4">
-            <img src={Alimentacion} alt="Alimentacion" className="card-image" />
-            <h3>Alimentacion</h3>
-            <p>Información específica sobre la alimentación saludable.</p>
+            <img src={Alimentacion} alt={props.name} className="card-image" />
+            <h3>{props.name}</h3>
+            <p>{props.descripcion}</p>
+            <p>{props.saludarfunc()}</p>
         </div>
         )
     }
+
+function saludar(){
+    return(
+        <div>
+            <h2>Hola tonotos</h2>
+        </div>
+    )
+}
 
 export default Cards
