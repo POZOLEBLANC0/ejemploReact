af8a72da28a991929043354e6fc14086d26556ff
POZOLEBLANC0 <240597@utxicotepec.edu.mx>
2026-02-13 09:49:35 -0600

    ubi actual

--- pozole/src/footer.jsx
+++ pozole/src/footer.jsx
@@ -1,14 +1,10 @@
-import Mapa from './mapa'
+import MapaGeolocalizacion from './mapaGeolocalizacion'
 import './footer.css'
 function Footer() {
     return (
         <footer className="footer-container">
             <p>© 2024 Pozole Inc. Todos los derechos reservados.</p>
-            <Mapa
-                lat={20.172013685180865}
-                lng={-98.04592930158009}
-                nombre="Pozole Central"
-            />
+            <MapaGeolocalizacion/>
         </footer>
     )
 }
